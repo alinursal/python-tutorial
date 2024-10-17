986af6359c87ce1e9c7704d1b408c9ca8ae9d9fe
alinursal <hanursal@gmail.com>
2024-10-17 15:07:14 -0400

    init commit

--- mini-project-the-fibonacci-sequence/main.py
+++ mini-project-the-fibonacci-sequence/main.py
@@ -1,6 +1,6 @@
 def main():
     count = int(input("Enter the number of Fibonacci terms to print: "))
-    a, b = 1, 2
+    a, b = 0, 1
     print("Fibonacci sequence:")
     for _ in range(count):
         print(a, end=" ")
